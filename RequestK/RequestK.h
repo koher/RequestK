ee09eaf4a17daf570e5d0587698735fc7717d2b8
Yuta Koshizawa <koher@koherent.org>
2016-01-20 10:22:07 +0900

    Initial commit

---
+++ RequestK/RequestK.h
@@ -0,0 +1,19 @@
+//
+//  RequestK.h
+//  RequestK
+//
+//  Created by Yuta Koshizawa on 1/14/16.
+//  Copyright © 2016 koherent.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RequestK.
+FOUNDATION_EXPORT double RequestKVersionNumber;
+
+//! Project version string for RequestK.
+FOUNDATION_EXPORT const unsigned char RequestKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestK/PublicHeader.h>
+
+
